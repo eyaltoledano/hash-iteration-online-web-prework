c12dbc62be58d67a64e7635f1ee9faa0c7954640
Eyal Toledano <eyaltoledano@me.com>
2019-02-01 04:26:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,5 +8,5 @@ def happy_birthday(birthday_kids)
   greeting = birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!\n"
   end
-  greeeting
+  greeting
 end
